c2a80a6a013eb5b4effc47955a1e472f99b473f3
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2022-06-21 08:04:01 +0000

    test: Update Test Suite

--- tests/nn/test_optimizers.py
+++ tests/nn/test_optimizers.py
@@ -1,20 +1,19 @@
 """Basic test to check 8-bit Optimizers"""
 from __future__ import annotations
 
-import bitsandbytes as bnb
 import torch
 
 
-def test_adamw8bit():
+def test_adamw():
     """
     Simple test to check Installation and Instantiation
     """
-    params = torch.nn.Parameter(torch.rand(10, 10).cuda())
-    constant = torch.rand(10, 10).cuda()
+    params = torch.nn.Parameter(torch.rand(10, 10))
+    constant = torch.rand(10, 10)
 
     original = params.data.sum().item()
 
-    adamw = bnb.optim.AdamW8bit([params])
+    adamw = torch.optim.AdamW([params])
 
     out = constant * params
     loss = out.sum()
